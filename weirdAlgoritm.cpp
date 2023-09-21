6fdf1ae5c5ad2663234bfadd5f7db8d8c10901dd
replyre <121796450+replyre@users.noreply.github.com>
2023-09-22 01:14:11 +0530

    problem : weird Algoritm

---
+++ weirdAlgoritm.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+
+// freopen('input.txt', 'r', stdin);
+// freopen('output.txt', 'w', stdout);
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n;
+    cin >> n;
+    while (n > 1)
+    {
+        cout << n << " ";
+        if (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            n = 3 * n + 1;
+        }
+    }
+    cout << 1;
+
+    return 0;
+}
